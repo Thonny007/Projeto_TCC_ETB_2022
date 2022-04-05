7bffef144aba4717751f6323cd4c394fbc21bb55
Lucas Martins <lukita.m.s@gmail.com>
2022-04-05 19:00:13 -0300

    mais um teste

--- Projeto_TCC2/administracao.php
+++ Projeto_TCC2/administracao.php
@@ -1,30 +1,20 @@
-<?php include "logo_menu.php"?>
-<?php /* 	
+<?php include "logo_menu.php"?> 	
 <ul>		
 	<li> <a href="administracao.php" > Adminitração </a> </li>
 	<li> <a href="lista_cadastro.php" > Cadastros </a> </li>
 	<li> <a href="lista_agendamentos" > Agendamento </a> </li>
 	
 </ul>
-<?php
  //else if ($_SESSION["id_cliente"] == "cliente"){
-?>
 <ul>
 	<!-- lista o agendamento do cliente-->
 	<li><a href="lista_agendamento"></a></li>
     <li> <a href="agendamento.php" > Agendar Tattoo </a> </li>  
     <li> <a href="orçamento.php"> Orçamento </li>
 </ul>
-<?php 
-
- /*else { */
-?> 
 <!--
  <ul>		
 	<li> <a href="administracao.php" > AdminitraAdminitração </a> </li>
 	<li> <a href="vendas.php" > Vendas </a> </li>
 </ul>
-</*?php
-
-?>
 -->

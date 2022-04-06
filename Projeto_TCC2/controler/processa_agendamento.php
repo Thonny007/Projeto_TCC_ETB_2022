a5cd980c55dbda0571faf560f6c0887bd1d21237
Lucas Martins <lukita.m.s@gmail.com>
2022-04-06 08:27:52 -0300

    processa agendamento

---
+++ Projeto_TCC2/controler/processa_agendamento.php
@@ -0,0 +1,12 @@
+<?php
+    include_once "../classes/Agendamento.php";
+    
+    $data = $_POST["data_agendamento"];
+    $hora = $_POST["hora_agendamento"];            
+    $data_completa = "$data $hora";
+    $formataData = new DateTime($data_completa);
+    $formataData = $formataData->format('Y-m-d H:i:s');
+
+    $desc = $_POST["desc"];
+    var_dump($_POST['ft']);
+?>

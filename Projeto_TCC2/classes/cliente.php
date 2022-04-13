c6ace5199d1843942b7978cc8bc94482b00f1e73
Thonny007 <103069758+Thonny007@users.noreply.github.com>
2022-04-13 13:20:35 -0300

    ATUALIZAÇÃO

--- Projeto_TCC2/classes/cliente.php
+++ Projeto_TCC2/classes/cliente.php
@@ -51,7 +51,7 @@ class Cliente {
         
     }
 
-    public static function list(): mysqli_result | bollean  {
+    public static function list(): mysqli_result /* |boll */ {
         $con = mysqli_connect("localhost", "root", "", "agendamentos");
 
         $sql_consulta = "SELECT id_clt, nome_clt, login_clt, senha_clt, 

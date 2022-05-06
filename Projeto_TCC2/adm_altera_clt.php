bbf35af448a20887047e942a2a4b3ccf8135f439
Thonny007 <carlosantoniodesouzaneri@gmail.com>
2022-05-06 20:40:13 -0300

    Tentei fazer o ADM altera os cadatro dos CLT's 06/05

--- Projeto_TCC2/adm_altera_clt.php
+++ Projeto_TCC2/adm_altera_clt.php
@@ -11,39 +11,45 @@
         <link rel="stylesheet" type="text/css" href="css/layout.css">
 		<link rel="stylesheet" type="text/css" href="css/menu.css">
         <link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
-    </head>
-    <body>					
-					<?php
-						include "menu_entrada.php";
-					?>
-				
-				<div id="funcionalidade" class="div_direita">
-					<?php
-						$con = mysqli_connect ("localhost", "root", "", "agendamentos");
-						
-						$cod = $_GET["id"];
-										
-						$sql_pesquisa = "SELECT  id_clt,nome_clt, login_clt, senha_clt,data_nascimento, numero_cliente
-										 FROM clientes
-										 WHERE id_clt = '$cod'";
-						$resultado_pesquisa = mysqli_query ($con, $sql_pesquisa);	
-						
-						$registro = mysqli_fetch_row($resultado_pesquisa);
-					?>
-					<form>
-									<input type="hidden" name="id" value="<?php echo $registro[0]?>">
-
-									<div class="form-group">
-										<label>Nome</label>
-										<input type="text" class="form-control" name="nome" value="<?php echo $registro[1] ?>">
-									</div>
-									<div class="form-group">
-										<label>Data de Nascmento</label>
-										<input type="date" class="form-control" name="data_nasc" value="<?php echo $registro[4]; ?>">
-									</div>
-									<div class="form-group">
-										<label>Telefone</label>
-										<input type="text" class="form-control" name="telefone" value="<?php echo $registro[7] ?>">
+		<style>
+        form{
+            width: 50%;
+            margin: auto;
+            background-color:#808080;
+            font-size: 20px;
+            border-radius:10px;
+            color: white;
+            text-align:center;
+        }
+    </style>
+	</head>
+    <body>
+		<?php
+			include "menu_entrada.php";
+		?>
+		<div id="funcionalidade" class="div_direita">
+			<?php
+				$con = mysqli_connect ("localhost", "root", "", "agendamentos");		
+				$cod = $_GET["id"];
+				$sql_pesquisa = "SELECT  id_clt,nome_clt, login_clt, senha_clt,data_nascimento, numero_cliente
+									FROM clientes
+									WHERE id_clt = '$cod'";
+				$resultado_pesquisa = mysqli_query ($con, $sql_pesquisa);	
+				$registro = mysqli_fetch_row($resultado_pesquisa);
+			?>
+			<form>
+				<input type="hidden" name="id" value="<?php echo $registro[0]?>">
+				<div class="form-group">
+					<label>Nome</label>
+					<input type="text" class="form-control" name="nome" value="<?php echo $registro[1] ?>">
+				</div>
+				<div class="form-group">
+					<label>Data de Nascmento</label>
+					<input type="date" class="form-control" name="data_nasc" value="<?php echo $registro[4]; ?>">
+				</div>
+				<div class="form-group">
+					<label>Telefone</label>
+					<input type="text" class="form-control" name="telefone" value="<?php echo $registro[7] ?>">
 									</div>
 									<div class="form-group">
 										<label>Login</label>
--- Projeto_TCC2/altera_clt.php
+++ Projeto_TCC2/altera_clt.php
@@ -36,46 +36,43 @@
         }
     ?>
     <div id="funcionalidade" class="div_direita">
-					<form method="post" action="controler/processa_altera_clt.php">
-						<?php 
-							if ($registro != null) { 
-						?>
-							<div class="container">
-								<form>
-									<input type="hidden" name="id" value="<?php echo $registro[0]?>">
-
-									<div class="form-group">
-										<label>Nome</label>
-										<input type="text" class="form-control" name="nome" value="<?php echo $registro[1] ?>">
-									</div>
-									<div class="form-group">
-										<label>Data de Nascmento</label>
-										<input type="date" class="form-control" name="data_nasc" value="<?php echo $registro[4]; ?>">
-									</div>
-									<div class="form-group">
-										<label>Telefone</label>
-										<input type="text" class="form-control" name="telefone" value="<?php echo $registro[7] ?>">
-									</div>
-									<div class="form-group">
-										<label>Login</label>
-										<input type="text" class="form-control" name="login" value="<?php echo $registro[2] ?>">
-									</div>
-									<div class="form-group">
-										<label >Senha</label>
-										<input type="password" class="form-control" name="senha" value="<?php echo $registro[3]?>">
-									</div>
-									<button type="submit" class="btn btn-danger">alterar</button>
-								</form>
-							</div>
-						<?php
-							}
-							else {
-								echo "alert(' erro ao consultar ')";
-							}
-						?>
-					</form>
+		<form method="post" action="controler/processa_altera_clt.php">
+			<?php 
+				if ($registro != null) { 
+			?>
+			<div class="container">
+				<form>
+					<input type="hidden" name="id" value="<?php echo $registro[0]?>">
+					<div class="form-group">
+						<label>Nome</label>
+							<input type="text" class="form-control" name="nome" value="<?php echo $registro[1] ?>">
+					</div>
+					<div class="form-group">
+						<label>Data de Nascmento</label>
+						<input type="date" class="form-control" name="data_nasc" value="<?php echo $registro[4]; ?>">
+					</div>
+					<div class="form-group">
+						<label>Telefone</label>
+						<input type="text" class="form-control" name="telefone" value="<?php echo $registro[7] ?>">
+					</div>
+						<div class="form-group">
+						<label>Login</label>
+						<input type="text" class="form-control" name="login" value="<?php echo $registro[2] ?>">
+					</div>
+					<div class="form-group">
+						<label >Senha</label>
+						<input type="password" class="form-control" name="senha" value="<?php echo $registro[3]?>">
+					</div>
+						<button type="submit" class="btn btn-danger">alterar</button>
+				</form>
 				</div>
-  
-		
-    </body>
+			<?php
+					}else {
+						echo "alert(' erro ao consultar ')";
+							}
+				?>
+		</form>
+			
+	</div>
+</body>
 </html>

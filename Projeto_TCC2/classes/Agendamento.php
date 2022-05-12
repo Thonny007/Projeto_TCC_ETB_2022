a1ccf65dd6530e96c7e0da8747cdc75985daf499
Lucas Martins <lukita.m.s@gmail.com>
2022-05-12 12:00:50 -0300

    alterando a classe Agendamentos

--- Projeto_TCC2/classes/Agendamento.php
+++ Projeto_TCC2/classes/Agendamento.php
@@ -1,26 +1,45 @@
 <?php
 
 class Agendamentos {
-    private $data_agendamento;
-    private $status_agendamento;
+    private $id;
+    private $data;
+    private $status;
     private $con;
     private $imagem;
     private $desc;
 
-    function __construct($data_agendamento, $imagem, $desc) {
-        $this->data_agendamento = $data_agendamento;
+    function __construct(
+        $id = null,
+        $data = null,
+        $status = null,
+        $imagem = null,
+        $desc = null
+    ) {
+        $this->Agendamentos($id, $data, $status, $imagem, $desc);
+    }
+
+
+    private function Agendamentos(
+        $id,
+        $data,
+        $status,
+        $imagem,
+        $desc
+    ){
+        $this->id = $id;
+        $this->data = $data;
         $this->desc = $desc;
         $this->imagem = $imagem;
+        $this->status = $status;
         $this->con = mysqli_connect("localhost", "root", "", "agendamentos");
     }
 
-    
 
     public function insert(){
         $sql = "INSERT INTO agendamento
             (data_agnd, imagem_atendimento, descricao_tatto)
         VALUES
-            ('$this->data_agendamento','$this->imagem','$this->desc')";
+            ('$this->data','$this->imagem','$this->desc')";
 
         mysqli_query($this->con, $sql);
 
@@ -30,18 +49,16 @@ class Agendamentos {
 
         $queryLastId = mysqli_query($this->con, $getLastId);
         $resultLastId = mysqli_fetch_row($queryLastId);
-        
+
         mysqli_close($this->con);
 
         return $resultLastId;
     }
 
-    public static function list($excluidos = false ): mysqli_result /* | bollean */{
-        $con = mysqli_connect("localhost", "root", "", "agendamentos");
-        
+    public function list($excluidos = false ){
         if (!$excluidos) {
-            $sql_consulta = 
-            "SELECT 
+            $sql_consulta =
+                "SELECT 
                 ag.id_agnd as 'id', 
                 ag.data_agnd as 'data', 
                 ag.status_agnd as 'status', 
@@ -52,8 +69,8 @@ class Agendamentos {
             inner join cliente c on c.id_agnd  = ag.id_agnd
             where ag.status_agnd  = 'espera'";
         } else {
-            $sql_consulta = 
-            "SELECT 
+            $sql_consulta =
+                "SELECT 
                 ag.id_agnd as 'id', 
                 ag.data_agnd as 'data', 
                 ag.status_agnd as 'status', 
@@ -64,107 +81,90 @@ class Agendamentos {
             inner join cliente c on c.id_agnd  = ag.id_agnd
             where ag.status_agnd = 'desmarcado' ";
         }
-    
-        return $result = mysqli_query ($con, $sql_consulta);
+
+        return mysqli_query ($this->con, $sql_consulta);
     }
 
-    public static function getImgById($id){
-        $con = mysqli_connect("localhost", "root", "", "agendamentos");
-        $sql = "SELECT id_agnd FROM agendamento WHERE id_agnd = $id";
-        
-        return mysqli_query($con, $sql);
+    public function getImg(){
+        $sql = "SELECT id_agnd FROM agendamento WHERE id_agnd = $this->id";
+        $query = mysqli_query($this->con, $sql);
+
+        return mysqli_fetch_row($query);
     }
-    public function delete($id){
-        $sql = "DELETE FROM agendamento WHERE id_agnd = '$id'";
+    public function delete(){
+        $sql = "DELETE FROM agendamento WHERE id_agnd = '$this->id'";
 
         try {
-            $query = mysqli_query($this->con, $sql);
-            
+            mysqli_query($this->con, $sql);
+
             echo "
                 <script>
                     alert ('☺ Registro Deletado/Apagado com Sucesso ☺') 
                 </script>
             ";
-            
+
             echo "
             <script> 
-                location.href = ('../lista_agendamento_excluido.php') 
+                location.href = '../lista_agendamento_excluido.php' 
             </script>";
 
-            
-        } catch (\Throwable $th) {
+
+        } catch (Throwable $th) {
             echo "
             <script> 
                 alert ('ERRO AO DELETAR DADO') 
             </script>";
-            
+
             echo "
             <script> 
-                location.href ('../lista_agendamento_excluido.php') 
+                location.href = '../lista_agendamento_excluido.php' 
             </script>";
-            
+
         }
 
 
     }
 
-    public static function getById($id, $toAgnd=false) {
-        $con = mysqli_connect("localhost", "root", "", "agendamentos");
-        
+    public function getById($id, $toAgnd=false) {
+
         $sql = "SELECT * FROM agendamento WHERE id_agnd = $id";
-        $query = mysqli_query($con, $sql);
-        
+        $query = mysqli_query($this->con, $sql);
+
         $result = mysqli_fetch_row($query);
-        
+
         $agnd = new Agendamentos (
+            $result[0],
             $result[1],
             $result[2],
             $result[3],
             $result[4],
-            $result[5],
         );
 
-       return $toAgnd ? $agnd : $result;
+        return $toAgnd ? $agnd : $result;
     }
 
-    public static function updateStatus($id, $new_status){
-        $con = mysqli_connect("localhost", "root", "", "agendamentos");
-        
-        $sql = 
-        "UPDATE agendamento SET status_agnd='$new_status'
-	    WHERE id_agnd=$id;";
+    public function updateStatus($new_status){
+        $this->setStatus($new_status);
 
-        mysqli_query($con, $sql);
-        mysqli_close($con);
+        $sql = "UPDATE agendamento SET status_agnd='$this->status'
+	    WHERE id_agnd=$this->id;";
+
+        mysqli_query($this->con, $sql);
+        mysqli_close($this->con);
     }
-    
+
+    /**
+     * @throws Exception
+     */
     public static function formataData(string $dataRecebida): string{
         $data = new DateTime($dataRecebida);
-        
+
         return $data->format('d/m/Y H:i');
     }
 
-    function getData_agendamento() {
-        return $this->data_agendamento;
+    public function setStatus($status): void{
+        $this->status = $status;
     }
 
-    function getStatus_agendamento() {
-        return $this->status_agendamento;
-    }
 
-    function setData_agendamento($data_agendamento) {
-        $this->data_agendamento = $data_agendamento;
-    }
-
-    function setStatus_agendamento($status_agendamento) {
-        $this->status_agendamento = $status_agendamento;
-    }
-
-    public function getImagem(){
-        return $this->imagem;
-    }
-
-    public function setImagem($imagem){
-        $this->imagem = $imagem;
-    }
 }

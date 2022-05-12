5da6c67ea7fbcd336e9773b961ae06e080ea19b2
Thonny007 <103069758+Thonny007@users.noreply.github.com>
2022-05-12 10:33:03 -0300

    ADD 12/05 ====== às 10:32hrs
    ADD 12/05 ====== às 10:32hrs

--- Projeto_TCC2/rodape.php
+++ Projeto_TCC2/rodape.php
@@ -7,82 +7,72 @@
         <table style="width:100%;">
             <tr>
                 <td>
-                    <ol>
-                        <li>
-                            <h5> <U> CONTADOS </U> </h5>
-                        </li>
-                        <li>
+                    <dl id="contatos_menu" class="contatos_menu">
+                        <h5> <U> CONTATOS </U> </h5>
+                        <dd>
                             <a href="https://www.instagram.com/dite.tattoo/" target="_blank"> 
                                 <img src="img/contados/insta.png">
                                 Instagram
                             </a>
-                        </li>
-                        <li>
+                        </dd>
+                        <dd>
                             <a href="https://www.facebook.com/dite.tattoo" target="_blank">
                                 <img src="img/contados/facebook.png">
                                 Facebook
                             </a>
-                        </li>
-                        <li>
+                        </dd>
+                        <dd>
                             <img src="img/contados/gmail.png" />
                             <a href="mailto:clarice.terra42@gmail.com?subject=(ORÇAMENTO TATOO) &body=(Bom Dia, Boa Tarde/Noite)">
                                 clarice.terra42@gmail.com
                             </a>
-                        </li>
-                    </ol>
+                        </dd>
+                    </dl>
                 </td>
                 <td style="width: 40%;">
-                    <ol>
+                    <dl>
                         <h5> <U> MENU </U> </h5>
-                        <li>
+                        <dd>
                             <a href="cadastro.php">
                                 <img src="img/menu/cadastro.png" />
                                 Cadastro
                             </a>
-                        </li>
-                        <!-- <li>
-                            <a href="agendamento.php">
-                                <img src="img/menu/calendar.png" />
-                                Agendamento
-                            </a>
-                        </li> -->
-                        <li>
+                        </dd>
+                        <dd>
                             <a href="orçamento.php">
                                 <img src="img/menu/orçamento.png" />
                                 Orçamento
                             </a>
                         </li>
-                        <li>
+                        <dd>
                             <a href="desenhos.php">
                                 <img src="img/menu/desenho.png">
                                 Portfólio
                             </a>
-                        </li>
-                        <li>
+                        </dd>
+                        <dd>
                             <a href="login.php">
-                                <img src="img/menu/login.png">
+                                <img width="60px" src="img/menu/login.png">
                                 LOGIN
                             </a>
-                        </li>
-                    </ol>
+                        </dd>
+                    </dl>
                 </td>
                 <td width="20%">
-                    <ol>
-                        <li>    
-                            <h5> 
-                                <U> ENDEREÇO </U> 
-                            </h5> 
-                            <h6>
-                                Localização do Estúdio
-                            </h6>
-                        </li>
-                        <li class="ender">
+                    <dl>
+                        <h5> 
+                            <U> ENDEREÇO </U> 
+                        </h5> 
+                        <h6>
+                             Localização do Estúdio
+                        </h6>
+                        <dd class="ender">
                             <a href="https://maps.app.goo.gl/kXXQeBLzujkS4NUk9" target="_blank"> 
-                                <img src="img/menu/map.png">
+                                <img class="map" src="img/menu/map.png">
                             </a>
-                            <img  src="img/qr_localizacao.png">
-                        </li>
-                    </ol>
+                            <img  src="img/qr_localizacao.jpg">
+                        </dd>
+                    </dl>
                 </td>
                 <td>
                     <img class="apresenta" src="img/Clarice_Maria_Terra.jpg" alt="" />

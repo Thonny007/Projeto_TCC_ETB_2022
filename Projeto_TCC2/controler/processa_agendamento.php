018949a9e957762cb6e6404294fc0bb6f050b954
Thonny007 <103069758+Thonny007@users.noreply.github.com>
2022-05-24 14:33:43 -0300

    ADD 24/05 ----- ÀS 14:35HRS
    ADD 24/05 ----- ÀS 14:35HRS

--- Projeto_TCC2/controler/processa_agendamento.php
+++ Projeto_TCC2/controler/processa_agendamento.php
@@ -1,5 +1,5 @@
 <?php
-
+    session_start();
     include_once "../classes/Agendamento.php";
     include_once "../classes/cliente.php";
 
@@ -10,14 +10,38 @@
     $formataData = new DateTime($data_completa);
     $formataData = $formataData->format('Y-m-d H:i:s');
     $desc = $_POST["desc"];
-
+/* 
     $path = "imgs_atendimento/". time() .$_FILES["ft"]['name'];
     $arquivo_tmp = $_FILES['ft']['tmp_name'];
 
+    move_uploaded_file($arquivo_tmp, $path); */
+if($_SESSION['adm']){
+    
+    $path = "imgs_atendimento/". time() .$_FILES["ft"]['name'];
+    $arquivo_tmp = $_FILES['ft']['tmp_name'];
     move_uploaded_file($arquivo_tmp, $path);
-
     $agendamento = new Agendamentos(null, $formataData, null, $path, $desc);
     $id_agnd = $agendamento->insert();
 
     $cliente = Cliente::getById($id, true);
     $cliente->geraRelacionamento($id_agnd);
+    echo"<script>
+            alert ('AGENDAMENTO REALISADO COM SUCESSO')
+            location.href = ('/Projeto_TCC2/lista_agendamentos.php')
+        </script>";
+}else{
+       
+    $path = "imgs_atendimento/". time() .$_FILES["ft"]['name'];
+    $arquivo_tmp = $_FILES['ft']['tmp_name'];
+    move_uploaded_file($arquivo_tmp, $path);
+    $agendamento = new Agendamentos(null, $formataData, null, $path, $desc);
+    $id_agnd = $agendamento->insert();
+
+    $cliente = Cliente::getById($id, true);
+    $cliente->geraRelacionamento($id_agnd);
+
+    echo"<script>
+            alert ('AGENDAMENTO REALISADO COM SUCESSO')
+            location.href = ('/Projeto_TCC2/ver_agendamento_clt.php')
+        </script>";
+}

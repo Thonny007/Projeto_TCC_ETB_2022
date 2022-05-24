0d4957437492fd426cb561b308c6ad3bec336bc9
Thonny007 <103069758+Thonny007@users.noreply.github.com>
2022-05-24 19:45:05 -0300

    ADD 24/05 -----ÀS 19:45HRS
    ADD 24/05 -----ÀS 19:45HRS

--- controler/processa_altera_clt.php
+++ controler/processa_altera_clt.php
@@ -8,11 +8,21 @@
     $data_nasc = $_POST["data_nasc"];
     $telefone = $_POST["telefone"];
     $cliente = new Cliente ($id, $nome, $data_nasc, $telefone,$senha, $login);
-
-        if(strlen($_POST["senha"]) < 6){
+    
+    $hoje = new DateTime();
+    $ano_nascimento = explode("-",  $_POST["data_nasc"])[0];
+    $idade = $hoje->format('Y') - $ano_nascimento;
+  
+if ($_SESSION['adm']){
+    if($idade <= 17){
+            echo "<script>
+                alert ('⚠️🔞 Você ainda é Jovem para realiza uma Tattoo 🔞⚠️')
+                location.href = ('/Projeto_TCC2/lista_cadastro.php')
+            </script>";
+    }else if(strlen($_POST["senha"]) < 6){
 		    echo "<script>
 			    	alert ('⚠️ Sua senha teve ter no minimo 6(seis) caracteres ⚠️')
-				    location.href = ('../Projeto_TCC2/lista_cadastro.php')
+				    location.href = ('/Projeto_TCC2/lista_cadastro.php')
 			    </script>";
         }else{            
             $cliente->update($_SESSION['adm']);
@@ -21,5 +31,23 @@
 				    location.href = ('/Projeto_TCC2/lista_cadastro.php')
 			    </script>";
         }
-    
+}else{
+    if($idade <= 17){
+        echo "<script>
+            alert ('⚠️🔞 Você ainda é Jovem para realiza uma Tattoo 🔞⚠️')
+            location.href = ('/Projeto_TCC2/adm_cliente.php')
+        </script>";
+}else if(strlen($_POST["senha"]) < 6){
+        echo "<script>
+                alert ('⚠️ Sua senha teve ter no minimo 6(seis) caracteres ⚠️')
+                location.href = ('/Projeto_TCC2/altera_clt.php')
+            </script>";
+    }else{            
+        $cliente->update($_SESSION['adm']);
+        echo "<script>
+                alert ('☺ CADASTRO DE USUÁRIO ALTERADO COM SUCESSO ☺')
+                location.href = ('/Projeto_TCC2/adm_cliente.php')
+            </script>";
+    }
+}
 ?>

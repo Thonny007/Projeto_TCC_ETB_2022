46dff7e5068e9dc8c83c3a1e3812e281f7cfae36
Thonny007 <103069758+Thonny007@users.noreply.github.com>
2022-05-27 23:36:08 -0300

    Delete Projeto_TCC directory

--- Projeto_TCC/js/form_cadastro.js
+++
@@ -1,6 +0,0 @@
-const table_clt = $('#table_clt');
-const btn = $('#btn_mostrar');
-table_clt.hide();
-btn.on('click', () => {
-    table_clt.toggle();
-});
--- Projeto_TCC/js/table_clientes.js
+++
@@ -1,6 +0,0 @@
-const table = $('#table_cliente');
-const btn = $('#btn_mostrar');
-table.hide();
-btn.on('click', () => {
-    table.toggle();
-});
